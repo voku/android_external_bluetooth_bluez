b5056ba389d57dbe443c31cc4d61dcefb9fc13d7
Steve Kondik <shade@chemlab.org>
2010-06-04 19:16:33 -0400

    Cleanup makefile and build dund also.
    Change-Id: I2d757ae551ccccd8d56d682ef88e66099677c8c2

--- compat/Android.mk
+++ compat/Android.mk
@@ -1,39 +1,43 @@
 BUILD_PAND:=1
 BUILD_HIDD:=1
+BUILD_DUND:=1
+
+common_C_INCLUDES:= \
+        $(LOCAL_PATH)/../include \
+        $(LOCAL_PATH)/../common \
+        $(LOCAL_PATH)/../gdbus \
+        $(LOCAL_PATH)/../src \
+        $(call include-path-for, glib) \
+        $(call include-path-for, dbus)
+
+common_CFLAGS:= \
+        -DVERSION=\"4.47\" \
+        -DSTORAGEDIR=\"/data/misc/bluetoothd\" \
+        -DCONFIGDIR=\"/etc/bluez\" \
+        -DANDROID \
+        -DNEED_PPOLL \
+        -D__S_IFREG=0100000  # missing from bionic stat.h
+
+common_SHARED_LIBRARIES := \
+        libbluetooth \
+        libbluetoothd \
+        libdbus
 
 ifeq ($(BUILD_PAND),1)
 
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_C_INCLUDES:= \
-	$(LOCAL_PATH)/../include \
-	$(LOCAL_PATH)/../common \
-	$(LOCAL_PATH)/../gdbus \
-	$(LOCAL_PATH)/../src \
-	$(call include-path-for, glib) \
-	$(call include-path-for, dbus)
-
-LOCAL_CFLAGS:= \
-	-DVERSION=\"4.47\" \
-	-DSTORAGEDIR=\"/data/misc/bluetoothd\" \
-	-DCONFIGDIR=\"/etc/bluez\" \
-	-DANDROID \
-	-DNEED_PPOLL \
-	-D__S_IFREG=0100000  # missing from bionic stat.h
+LOCAL_C_INCLUDES := $(common_C_INCLUDES)
+LOCAL_CFLAGS := $(common_CFLAGS)
+LOCAL_SHARED_LIBRARIES := $(common_SHARED_LIBRARIES)
+LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
+LOCAL_MODULE := pand
 
 LOCAL_SRC_FILES:= \
-	pand.c    \
-	bnep.c    \
-	sdp.c
-
-LOCAL_SHARED_LIBRARIES := \
-	libbluetooth \
-	libbluetoothd \
-	libdbus
-
-LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
-LOCAL_MODULE:=pand
+        pand.c    \
+        bnep.c    \
+        sdp.c
 
 include $(BUILD_EXECUTABLE)
 endif
@@ -43,34 +47,36 @@ ifeq ($(BUILD_HIDD),1)
 
 include $(CLEAR_VARS)
 
-LOCAL_C_INCLUDES:= \
-	$(LOCAL_PATH)/../include \
-	$(LOCAL_PATH)/../common \
-	$(LOCAL_PATH)/../gdbus \
-	$(LOCAL_PATH)/../src \
-	$(call include-path-for, glib) \
-	$(call include-path-for, dbus)
-
-LOCAL_CFLAGS:= \
-	-DVERSION=\"4.47\" \
-	-DSTORAGEDIR=\"/data/misc/bluetoothd\" \
-	-DCONFIGDIR=\"/etc/bluez\" \
-	-DANDROID \
-	-DNEED_PPOLL \
-	-D__S_IFREG=0100000  # missing from bionic stat.h
+LOCAL_C_INCLUDES := $(common_C_INCLUDES)
+LOCAL_CFLAGS := $(common_CFLAGS)
+LOCAL_SHARED_LIBRARIES := $(common_SHARED_LIBRARIES)
+LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
+LOCAL_MODULE := hidd
 
 LOCAL_SRC_FILES:= \
 	hidd.c    \
 	sdp.c    \
 	fakehid.c
 
-LOCAL_SHARED_LIBRARIES := \
-	libbluetooth \
-	libbluetoothd \
-	libdbus
+include $(BUILD_EXECUTABLE)
+endif
 
+ifeq ($(BUILD_DUND),1)
+
+include $(CLEAR_VARS)
+
+LOCAL_C_INCLUDES := $(common_C_INCLUDES)
+LOCAL_CFLAGS := $(common_CFLAGS)
+LOCAL_SHARED_LIBRARIES := $(common_SHARED_LIBRARIES)
 LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
-LOCAL_MODULE:=hidd
+LOCAL_MODULE := dund
+
+LOCAL_SRC_FILES:= \
+        dund.c \
+        sdp.c \
+        dun.c \
+        msdun.c
 
 include $(BUILD_EXECUTABLE)
 endif
+
